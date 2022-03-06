3be46c450bbbc3905dc3a5b391733d3732ade145
Don-Heal <donnie.heal@gmail.com>
2022-03-06 17:34:48 +1100

    finished nav, sign in and sign out.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def alert_class(type)
+        case type
+        when "notice"
+            return "success"
+        when "alert"
+            return "warning"
+        when "error"
+            return "danger"
+        else
+            return "dark"
+        end
+    end
 end
