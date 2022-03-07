f3a7a4869e071befa966642abe5cedc8125e870b
Don-Heal <donnie.heal@gmail.com>
2022-03-07 14:43:14 +1100

    added categorys and items tables and model

---
+++ db/migrate/20220307032700_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :condition
+      t.text :description
+      t.integer :price
+      t.boolean :sold, default: false
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
