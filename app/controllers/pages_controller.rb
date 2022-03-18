6076c9eb95c631f039bc72ed4d16eefbadea6792
Don-Heal <donnie.heal@gmail.com>
2022-03-18 11:49:07 +1100

    fixing routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,7 @@ before_action :variables, :auth_user, only: [:home, :categories]
 
     def auth_user
         if @item.user_id != current_user.id
-            redirect_to home_page_path alert: "Please Sign In or Sign Up to do that."
+            redirect_to "/items/index" alert: "Please Sign In or Sign Up to do that."
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
 
-  root to: "pages#home" as: "home_page"
+  root to: "pages#home"
 
   
   get "items/index", to: "items#index", as: "items_home"
